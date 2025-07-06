001290f662c801fa0526178f9d8216bb46421a6b
Tyson.Lin <gn00667340@gmail.com>
2025-07-07 00:05:11 +0800

    test

--- Program.cs
+++ Program.cs
@@ -4,58 +4,103 @@ using System.Text;
 using MimeKit;
 using MailKit.Net.Smtp;
 
-var eventId = "5cee48b5";
-var seatId = 839018;
+var eventId = "hrtesd";
+var seatId = 869447;
 var queueUrl = $@"https://queue.kktix.com/queue/{eventId}"; // + ?authenticity_token
 var getParamUrl = $@"https://queue.kktix.com/queue/token/"; // + queue response Token
 var landingUrl = $@"https://kktix.com/events/{eventId}/registrations/"; // + Param response To_param
-var authenticity_token = @"4%2BSucpJWVmMuumIeI16OE0R7upU2IKHkrLmGBrmZSCs%2BxI41iTKDEeVTSJJi59abzxjZlRkGoVCc%2BBfqT7bEWQ%3D%3D";
-var cookie = @"_fbp=fb.1.1743575019240.411827062547173178; _hjSession_1979059=eyJpZCI6IjI2ZjQwZDdkLWQ3YWUtNGZmMi1iZGRmLTIxNWQzMDVhZjBiNyIsImMiOjE3NDM1NzUwMTkzODEsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjoxLCJzcCI6MH0=; _clck=18jokh0%7C2%7Cfuq%7C0%7C1918; _gid=GA1.2.1964172670.1743575020; _hjSessionUser_1979059=eyJpZCI6ImRjZjkyOThjLTM3N2ItNTAwMi05YzI2LTY3MjQ3NTY4YTNjNiIsImNyZWF0ZWQiOjE3NDM1NzUwMTkzODAsImV4aXN0aW5nIjp0cnVlfQ==; user_display_name_v2=%25E6%259E%2597%25E5%25AE%25B6%25E8%25B1%25AA; user_avatar_url_v2=https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2f000d241af9eef893674727f0a272dd.png; user_id_v2=451791; user_path_v2=%2Fuser%2F451791; user_time_zone_v2=Asia%2FTaipei; user_time_zone_offset_v2=28800; kktix_session_token_v2=3ad701528d379f6f603da6e0429733f3; mobileNotVerified=0; locale=zh-TW; _ga=GA1.2.97419787.1743575019; _ga_WZBYP4N1ZG=GS1.2.1743575019.1.1.1743575156.55.0.0; _clsk=1xdz372%7C1743575751952%7C14%7C0%7Ci.clarity.ms%2Fcollect; _gali=registrationsNewApp; _ga_LWVPBSFGF6=GS1.1.1743575019.1.1.1743576006.60.0.0; _ga_SYRTJY65JB=GS1.1.1743575019.1.1.1743576006.60.0.0; _ga_GCLEH6R9MF=GS1.1.1743575079.1.1.1743576006.60.0.0; XSRF-TOKEN=Ghk51hi7ET3Z6dMJymMTkVgZSwfaEpyvOdLWRms46DbHORmRA9%2FETxIA%2BYWL2ksZ03ooB%2FU0nBsJk0eqnRdkRA%3D%3D";
-var thread = 48;
-List<int> list = new();
-for (var i = 1; i <= thread; i++)
-    list.Add(i);
+var authenticity_token = @"5IATxLe41WWVUhNLI4E9UiZOtU%2BSQpiGPEPwefPHjGP8M1OGYLWLCcbSaEEZycEGcLusObz85DVzKN1A5tACyg%3D%3D";
+var cookie = @"kktix_session_token_v2=e93fb4aa3e89d30bddf3b3253e3fcabd;";
+//var thread = 2048;
+List<Task> list = new();
+//for (var i = 1; i <= thread; i++)
+//    list.Add(i);
 
 try
 {
-    var cts = new CancellationTokenSource();
-    await Parallel.ForEachAsync(list, new ParallelOptions { MaxDegreeOfParallelism = thread, CancellationToken = cts.Token }, async (item,token) => {
-        while (true)
+    using var cts = new CancellationTokenSource();
+    using var semaphore = new SemaphoreSlim(1, 1);
+    while (!cts.IsCancellationRequested)
+    {
+        await semaphore.WaitAsync(cts.Token);
+        list.Add(Task.Run(async () =>
         {
             try
             {
                 var model = new TicketRequest
                 {
                     recaptcha = new { },
-                    tickets = new List<Ticket>
-                    {
+                    tickets = [
                         new Ticket
-                        {
-                            id = seatId,
-                            quantity = 3,
-                        }
-                    }
+                            {
+                                id = seatId,
+                                quantity = 3,
+                            }
+                    ]
                 };
 
                 var queueResponse = JsonConvert.DeserializeObject<TicketResponse>(await PostApiAsync($@"{queueUrl}?authenticity_token={authenticity_token}", model));
                 if (!string.IsNullOrWhiteSpace(queueResponse?.result) || string.IsNullOrWhiteSpace(queueResponse?.token))
-                    continue;
+                    return;
                 var tokenResponse = JsonConvert.DeserializeObject<Queue>(await GetApiAsync(getParamUrl + queueResponse.token));
                 if (!string.IsNullOrWhiteSpace(tokenResponse?.result) || string.IsNullOrWhiteSpace(tokenResponse?.to_param))
-                    continue;
+                    return;
                 var url = landingUrl + tokenResponse.to_param;
                 await SendEmail(url);
-                break;
+                cts.Cancel();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message.ToString());
-                continue;
+                return;
             }
-        }
-        cts.Cancel();
-        return;
-    });
+            finally 
+            {
+                semaphore.Release();
+            }
+        }, cts.Token));
+    }
+
+    await Task.WhenAll(list);
+
+    //await Parallel.ForEachAsync(list, new ParallelOptions { MaxDegreeOfParallelism = thread, CancellationToken = cts.Token }, async (item, token) =>
+    //{
+    //    while (true)
+    //    {
+    //        try
+    //        {
+    //            var model = new TicketRequest
+    //            {
+    //                recaptcha = new { },
+    //                tickets = new List<Ticket>
+    //                {
+    //                    new Ticket
+    //                    {
+    //                        id = seatId,
+    //                        quantity = 3,
+    //                    }
+    //                }
+    //            };
+
+    //            var queueResponse = JsonConvert.DeserializeObject<TicketResponse>(await PostApiAsync($@"{queueUrl}?authenticity_token={authenticity_token}", model));
+    //            if (!string.IsNullOrWhiteSpace(queueResponse?.result) || string.IsNullOrWhiteSpace(queueResponse?.token))
+    //                continue;
+    //            var tokenResponse = JsonConvert.DeserializeObject<Queue>(await GetApiAsync(getParamUrl + queueResponse.token));
+    //            if (!string.IsNullOrWhiteSpace(tokenResponse?.result) || string.IsNullOrWhiteSpace(tokenResponse?.to_param))
+    //                continue;
+    //            var url = landingUrl + tokenResponse.to_param;
+    //            await SendEmail(url);
+    //            break;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            Console.WriteLine(e.Message.ToString());
+    //            continue;
+    //        }
+    //    }
+    //    cts.Cancel();
+    //    return;
+    //});
 }
 catch (Exception ex)
 {
